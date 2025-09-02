20c79e7f772d18944f0302957e0c548a7766bb49
Alexis Ogbeche <alexisogbeche47@gmail.com>
2025-09-02 14:36:32 +0100

    interger done

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// Integers
+
+
+$a = 40;
+$b = 20;
+$c = 4.56;
+
+//+ -/ *
+
+//echo $b + $c;
+//echo $b - $c;
+//echo $a / $b;
+
+//Area of Circle
+$radius = 6;
+$pi = 3.1425;
+
+$area = $pi * $radius**2;
+
+//echo $area;
+
+//Temp. coverter 
+$celsius = 200;
+
+$fahrenheit = $celsius * 9 / 5 + 32;
+
+echo $fahrenheit;
+
+
+
+
+
+?>
