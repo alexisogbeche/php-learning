21929b7dcd4b60a2fbea8f8c33420cf9da18e713
Alexis Ogbeche <alexisogbeche47@gmail.com>
2025-09-02 15:40:20 +0100

    learn how to use html in php

--- index.php
+++ index.php
@@ -1,26 +1,33 @@
 <?php
+// Example of Array in use
 
-// Arrays
-
-$names = ['Rot','John','Pato','Alexis'];
-
-//print_r($names[0]);
-
-//Adding a new value to an array
-
-array_push($names, 'Chibs');
-//print_r($names);
-
-
-// Delete a value from an array
-array_splice($names, 0, 1);
-//print_r($names);
-
-//updating an existing array value
-
-$names[1] = 'Patrick';
-print_r($names);
-
-
-
+$chocolate_cake =   [
+                        'Chocolate Cake',
+                        '45 min',
+                        'Cake made with Chocolate',
+                        '5634'
+                    ];
+// print_r($chocolate_cake);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-learning</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+<body>
+    <div class="container">
+
+        <h1> <?php  echo $chocolate_cake[0] ?></h1>
+        <p> ID: <?php  echo $chocolate_cake[3] ?></p>
+        <p>Duration: <?php  echo $chocolate_cake[1] ?></p>
+        <p>Description: <?php  echo $chocolate_cake[2] ?></p>
+
+    </div>
+</body>
+</html>
